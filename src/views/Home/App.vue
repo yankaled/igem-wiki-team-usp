511ffff501949df7ac64a177c56ecfa418232a0b
pedrohcb989 <pc-bonamin@hotmail.com>
2021-10-13 23:52:33 -0300

    Rascunho Home

--- src/views/Home/App.vue
+++ src/views/Home/App.vue
@@ -1,23 +1,319 @@
 <template>
   <div class="home">
     <Nav />
-    
-    TESTANDO
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class="column is-5">
+        <figure class="image is-128x128">
+          <img
+            src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+          />
+        </figure>
+      </div>
+      <div class="content column is-4">
+        <h2>Let.It.Bee.</h2>
+        <p>
+          We always talk about how important bees are to our ecosystem, but we
+          don't hear enough about how big of a part they play in our daily
+          lives!
+        </p>
+      </div>
+      <div class="column is-2"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class=" content column is-5">
+        <p>
+          <b
+            >Bees are essential for ecological preservation, around 90% of the
+            species in the Atlantic Forest and 30% in the Pantanal depend on
+            their action.</b
+          >
+        </p>
+        <img
+          src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+        />
+      </div>
+      <div class="content column is-4">
+        <img
+          src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+        />
+      </div>
+      <div class="column is-2"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class=" content column is-9">
+        <p>
+          <b
+            >These small workers are essential to world agriculture, virtually
+            all commercial plantations depend on pollination.</b
+          >
+        </p>
+      </div>
 
+      <div class="column is-2"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class="content column">
+        <p>
+          <b
+            >In Brazil alone, 8 cultures depend directly or indirectly on the
+            action of bees</b
+          >
+        </p>
+      </div>
+      <div class=" content column">
+        <p>
+          <b
+            >Pollinated crops generate 9.3 billion dollars in exports for the
+            brazilian agribusiness!</b
+          >
+        </p>
+      </div>
+      <div class="column is-2"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class="content column is-6">
+        <p>
+          <b
+            >And this also directly impacts global food security, because
+            without bees the variability of food available to the population
+            around the world is also reduced</b
+          >
+        </p>
+      </div>
+      <div class="content column is-5"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-offset-7">
+        <img
+          src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+        />
+      </div>
+    </section>
+    <section class="columns">
+      <div class="column"></div>
+      <div class=" content column is-9">
+        <h2 style="text-align: center ;">But bees are being endangered...</h2>
+        <p>
+          by the indiscriminate and irrational use of pesticides, generating
+          massive deaths of these very important small insects.
+        </p>
+      </div>
+
+      <div class="column"></div>
+    </section>
+    <section class="columns" style="background-color:rgba(255, 102, 0, 0.7);;">
+      <div class="column is-1"></div>
+      <div class=" content column is-4">
+        <br />
+        <img
+          src="https://2021.igem.org/wiki/images/a/a2/T--USP-Brazil--IGEM-USP-Home-Destruction.png"
+        />
+      </div>
+      <div class="content column is-4">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          <b
+            style="color:#040404;
+            font-size: 30px;"
+            >Beekeepers find half a billion dead bees in 3 months in Brazil</b
+          >
+        </p>
+        <br />
+        <br />
+        <p style="color:#fff">(Galileu Maganize, 2019)</p>
+      </div>
+      <div class="column is-1"></div>
+    </section>
+    <section class="columns" style="text-align: center; ">
+      <div class="column"></div>
+      <div class=" content column is-9">
+        <p>
+          The mass death of bees and even entire hives has grown exponentially
+          since the 1990s and this has been closely linked to acute poisoning
+          due to the misuse of pesticides, especially the neonicotinoids class.
+        </p>
+        <br />
+        <p>
+          <b
+            >It would be very easy to say that they are the villains of our
+            story, but it is far more complex than that</b
+          >
+        </p>
+        <p>
+          <b> . . . </b>
+        </p>
+        <p>
+          <b
+            >neonicotinoids are actually one of the safest classes of pesticides
+            for humans.</b
+          >
+        </p>
+      </div>
+
+      <div class="column"></div>
+    </section>
+    <section class="columns">
+      <div class="column is-1"></div>
+      <div class=" content column is-4">
+        <img
+          src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+        />
+        <p>
+          It would be very easy to say that they are the villains of our story,
+          but it is far more complex than that:
+        </p>
+      </div>
+      <div class=" content column is-4">
+        <br />
+        <p>
+          It would be very easy to say that they are the villains of our story,
+          but it is far more complex than that:
+        </p>
+        <img
+          src="https://2021.igem.org/wiki/images/1/16/T--USP-Brazil--IGEM-USP-Home-Bee.png"
+        />
+      </div>
+      <div class="column is-1"></div>
+    </section>
+    <section class="columns" style="text-align: center; ">
+      <div class="column "></div>
+      <div class=" content column is-9">
+        <p>
+          So to maintain productivity and ensure food security for the world
+          population, it is necessary to combine productive agriculture with the
+          preservation of bees.
+        </p>
+        <p>
+          Therefore, pesticides must be safe for consumption and managed
+          responsibly so as not to lead to acute poisoning of these insects.
+        </p>
+      </div>
+
+      <div class="column "></div>
+    </section>
+    <section
+      class="columns"
+      style="text-align: center; background-color:#FFB923 "
+    >
+      <div class="column "></div>
+      <div class=" content column is-9">
+        <h2 style="text-shadow: 2px 6px 4px #FFFDFD;">
+          And what would be the solution?
+        </h2>
+        <img
+          src="https://2021.igem.org/wiki/images/e/e0/T--USP-Brazil--IGEM-USP-Home-Solution.png"
+        />
+        <br />
+        <br />
+        <p>
+          We have developed a transgenic plant with an enzyme capable of
+          metabolizing the neonicotinoids in its pollen!
+        </p>
+        <p>
+          We used CYP6G1, which makes Drosophila melanogaster resistant to
+          neonicotinoids, and paired it with a specific promoter for the pollen
+          of our model plant to incorporate this mechanism into our chassis.
+        </p>
+        <p>
+          And keeping the action of our project directed only on the plant's
+          pollen, we guarantee the protection of both crops and pollinators!
+        </p>
+      </div>
+
+      <div class="column "></div>
+    </section>
+    <section class="columns">
+      <div class="column is-3"></div>
+
+      <div class=" content column is-2">
+        <br />
+        <br />
+        <br />
+
+        <img
+          src="https://2021.igem.org/wiki/images/3/37/T--USP-Brazil--IGEM-USP-Home-Bee-with-guitar.png"
+        />
+      </div>
+      <div class=" content column is-4">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          And when the night is cloudy<br />
+          There is still a light that shines on me<br />
+          Shine on until tomorrow<br />
+          Let it be
+          <br />
+          <br />
+          <b style="color:#046620"> Beatles</b>
+        </p>
+      </div>
+
+      <div class="column "></div>
+    </section>
     <Footer />
   </div>
 </template>
+<style scoped>
+.content h2 {
+  font-family: "Roboto", sans-serif;
+  font-size: 50px;
+  font-weight: 900;
+  color: #040404;
+  text-shadow: 2px 6px 4px rgba(159, 109, 0, 0.5);
+}
+.content h3 {
+  font-family: "Roboto", sans-serif;
+  font-size: 35px;
+  font-weight: 900;
+  color: #040404;
+  text-shadow: 2px 6px 4px rgba(159, 109, 0, 0.5);
+}
+.content p {
+  font-size: 22px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  color: #262625;
+}
+
+.content b {
+  color: #ffba17;
+}
+.content p b {
+  font-weight: 700;
+}
+.references {
+  background: #fffdfd;
+  border: 2px dashed #000000;
+  border-radius: 20px;
+  font-size: 22px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  color: #262625;
+}
+</style>
 
 <script>
-import Nav from '@/components/Nav.vue'
-import Footer from '@/components/Footer.vue'
+import Nav from "@/components/Nav.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
 
   components: {
     Nav,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
