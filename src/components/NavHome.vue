8add0d5112a02f8481471821bd5e6a8a138890db
Yan Kaled <yan.kaled.barbosa@gmail.com>
2021-10-18 16:07:11 -0300

    NavHome transition

--- src/components/NavHome.vue
+++ src/components/NavHome.vue
@@ -160,11 +160,12 @@ export default {
 
 #Navbar_USP {
   margin-bottom: -1em;
+  transition:all 0.5s;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);
 }
 
 .transparent-nav {
-  display: none !important;
+  background-color: transparent !important;
 }
 
 .nav-link-usp {
