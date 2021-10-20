6e17ce863a6473e3c0a85c9e7d59806523b27f3b
pedrohcb989 <pc-bonamin@hotmail.com>
2021-10-20 13:59:14 -0300

    Inicio Partnership2

--- src/views/Partnership/App.vue
+++ src/views/Partnership/App.vue
@@ -5,13 +5,228 @@
     <div class="is-full">
       <img
         class="header"
-        src="https://2021.igem.org/wiki/images/7/71/T--USP-Brazil--IGEM-USP-Sponsors-Banner.png"
+        src="https://2021.igem.org/wiki/images/9/9f/T--USP-Brazil--IGEM-USP-Public-Banner.jpg"
       />
     </div>
 
+    <Collapsible
+      title="Open Collapsible"
+      src="https://2021.igem.org/wiki/images/9/9f/T--USP-Brazil--IGEM-USP-Public-Banner.jpg"
+      text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    />
+
     <section class="columns">
-      <div class="column is-one-fifth"></div>
-      <div class="content column is-three-fifth"></div>
+      <div class="menu column is-one-fifth"></div>
+
+      <Backtotop />
+
+      <div class="content column is-three-fifth">
+        <!-- Heroes in Science -->
+        <section>
+          <h2>Heroes in Science</h2>
+          <h3>What is it</h3>
+          <p>
+            The <b>#HeroisNaCiencia</b> was an campaign realized in social media
+            by the team USP BR. This initiative consisted in inviting brazilian
+            (and some international) researchers to share, trough short clips,
+            how their research line where affected by the COVID-19 pandemic and
+            the actions taken to help the combat of the virus. The project also
+            tries to show the impacts of the pandemic on the day-to-day life of
+            the researchers, bringing them closer to society and humanizing
+            science.
+          </p>
+
+          <img
+            src="https://2021.igem.org/wiki/images/3/37/T--USP-Brazil--IGEM-USP-Public-Heroes.jpg"
+          />
+
+          <h3>History:</h3>
+          <p>
+            It all started when members of the group, whom sympathized with the
+            front-line professionals on the struggle against the coronavirus and
+            that, noting the whidespread desinformation across the country,
+            decided to contribute. With a team composed by 26 studants across
+            many institutes (Biology (IB), biomedical sciences (ICB),
+            pharmaceutical sciences (FCF), mathematics (IME) and Comunicantions
+            and arts (ECA) it was possible contacting the researchers, gather
+            reliable information and present them in a simpler way to society.
+            Thus the
+            <b> #HeroisNaCiencia </b>became one of the activities promoted by
+            the team.
+          </p>
+
+          <h3>Evolution:</h3>
+          <p>Among the projects publisheded, there are:</p>
+          <p></p>
+          <ul>
+            <li>studies on the structures of SARS-CoV-2,;</li>
+            <li>studies on the pathophysiology of Covid-19;</li>
+            <li>
+              the genetic sequencing of patients affected by this desease;
+            </li>
+            <li>the creation of biobanks;</li>
+            <li>development of study models with pulmonary cells,;</li>
+            <li>studies of diverse treatement methods;</li>
+            <li>
+              Among the projects publisheded, there are: production of
+              face-shields;
+            </li>
+            <li>
+              vaccine development; development and improovement of dyagnostic
+            </li>
+            <li>methods; actions on aplying dyagnostic on the frontline.</li>
+          </ul>
+          <img
+            src="https://2021.igem.org/wiki/images/2/23/T--USP-Brazil--IGEM-USP-Public-Heroes-post.jpg"
+          />
+
+          <p>
+            The first publication occured on May the eighth, on Instagram and
+            may the 15th on Facebook, but the cover for the campaign was only
+            released on May 22nd (both on Instagram and Facebook). By the end of
+            Heroes in Science (august 7th) 15 videos were posted, with the
+            participation of 17 researchers from 9 diferent institutions:
+          </p>
+          <img
+            src="https://2021.igem.org/wiki/images/7/71/T--USP-Brazil--IGEM-USP-Public-Heroes-Hexagon.jpg"
+          />
+          <p>
+            You can access the interviews on our Facebook or Instagram pages.
+            All of the videos are subtitled in english, so they can be watched
+            by more people around the world.
+          </p>
+        </section>
+
+        <section>
+          <h2>OBBS</h2>
+          <h3>The challenge</h3>
+          <img
+            src="https://2021.igem.org/File:T--USP-Brazil--img--image41.567c4a04.jpg"
+          />
+
+          <p>
+            During one of the Team’s earliest brainstorming sessions, we faced a
+            complicated challenge.
+
+            <b>How to make synbio known in Brazil? </b> <br /><br />
+            Many approaches were discussed, until we found a common ground for
+            the Team members. Call it a coincidence or not, but we had all
+            competed in several national and international academic olympiads
+            during High School, and had fond memories of that time in our lives.
+            Given that our team members come from all over the country, we
+            started thinking that
+            <b
+              >the creation of a national Synbio academic olympiad could be the
+              solution to how to popularize synthetic biology in our country.</b
+            >
+          </p>
+          <img
+            src="https://2021.igem.org/File:T--USP-Brazil--img--Group15.77cd2973.jpg"
+          />
+
+          <p>
+            Everyone was excited with the ideia, but we had no idea on how to
+            make such a gigantic event possible. So, the first thing we did was
+            to seek advice for former olympiad creators.
+          </p>
+          <h3>Learning from Experts</h3>
+          <p>
+            We talked to the coordinators of IYPT Brazil, a practical physics
+            competition, to see if there was any possibility to add experiments
+            to our own. However, that seemed impractical.
+          </p>
+          <p>
+            Then, we interviewed the coordinator of the Brazilian Physics
+            Olympiad (OBF), who helped us understand a little more about the
+            logistics of putting up a paper based national test. Even Though the
+            format of that competition resembled a little more what we wanted to
+            do, there was still something missing.
+          </p>
+          <p>
+            Physics is a mandatory subject in Brazilian High School, but
+            synthetic biology is not.
+            <b
+              >How could we make a test on a subject no one has ever studied?</b
+            >
+          </p>
+          <p>
+            João Canalle was the one who helped us work around that. Professor
+            Canalle did something very similar to what the team aimed to do.
+          </p>
+          <img
+            src="https://2021.igem.org/File:T--USP-Brazil--img--Group-14.75f82c4d.jpg"
+            alt=""
+          />
+
+          <p>
+            He created the Brazilian Astronomy and Astrophysics Olympiad to
+            popularize astronomy amog Brazilian students. Just like synbio,
+            astronomy isn’t taught in schools. Therefore, he had to come up with
+            a way to overcome this barrier.
+            <b
+              >His solution was to add as much information as he could in the
+              questions texts, so the students could learn while taking the
+              test.</b
+            >
+            That was the solution we were looking for.
+          </p>
+          <p>
+            Nevertheless, there was still a major challenge to surpass. All
+            those olympiads were paper based and demanded a logistic effort the
+            team couldn’t afford to put in the competition. We mentioned that to
+            professor Canalle, who then gave us the contact of a friend that he
+            thought had all the answers to our problems.
+          </p>
+          <img
+            src="https://2021.igem.org/File:T--USP-Brazil--img--Group-14.75f82c4d.jpg"
+            alt=""
+          />
+          <p>
+            That’s when we met <b>Daniel</b>. He, who’s also a professor, had
+            created several different olympiads in the same informative style as
+            professor Canalle. However,
+            <b>his tests had a little something extra: they were virtual.</b>
+            That not only solved the logistic issue but also opened up a world
+            of possibilities regarding media format. That meant we could add
+            authentic synbio material to our tests, including videos from iGEM
+            TV and other team’s wikis.
+          </p>
+          <h3>Ideate and design the Olympiads</h3>
+          <p>
+            After that interview we had our plan: To make a virtual,
+            informative, national olympiad in Brazil called:<br /><br />
+            <b>OBBS - Brazilian Synthetic Biology Olympiad!</b>
+          </p>
+          <p>
+            Shortly after we connected Fractal, a company specialized in digital
+            learning that had created the platforms for Daniel’s olympiads. They
+            were receptive to our idea and offered to partner with us through
+            their non-profit branch Instituto Vertere. Nonetheless, they
+            suggested we make a Pocket Olympiad to test our model before moving
+            to the grand national competition.
+          </p>
+          <p>
+            In the meantime, we contacted two professors from our university’s
+            Institute of Biology to help us understand the base for teaching
+            biological concepts by investigation
+          </p>
+          <h3>Launch the MVP</h3>
+          <p>
+            After months of preparation, In Jun 2021,
+            <b
+              >we launched the first ever synbio academic olympiad in Brazil:
+              The Hacking Ecosystems Pocket Olympiad!</b
+            >
+
+            <a
+              href="https://olimpiadapocket.com.br/olimpiada-pocket-hackeando-ecossistemas/"
+            >
+              <img
+                src="https://2021.igem.org/File:T--USP-Brazil--img--Group19.34d28dca.png"
+            /></a>
+          </p>
+        </section>
+      </div>
       <div class="column is-one-fifth"></div>
     </section>
 
@@ -23,21 +238,16 @@
 import Nav from "@/components/Nav.vue";
 import Footer from "@/components/Footer.vue";
 import Backtotop from "@/components/BacktoTop.vue";
+import Collapsible from "@/components/Collapsible.vue";
+
 export default {
-  name: "Safety",
+  name: "Education",
+
   components: {
     Nav,
     Footer,
     Backtotop,
+    Collapsible,
   },
 };
 </script>
-
-<style scoped>
-blockquote {
-  font-size: 22px;
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  color: #262625;
-}
-</style>
